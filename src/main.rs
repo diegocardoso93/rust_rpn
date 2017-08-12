8fe2691ee4099f97a00af52ac02033dc6d034579
dieg0 <dieg0card0s0@hotmail.com>
2017-08-12 20:00:37 -0300

    "2 3 4 + /" == "((4+3)/2)"

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 mod rpn;
 
 fn main() {
-    println!("Hello, world!");
-    let expr = " 2 3 4 + 2 /";
-    rpn::rpn_to_infix(expr);
+    let expr = "2 3 4 + /";
+    let infix_equation = rpn::rpn_to_infix_equation(expr);
+    println!("{}", infix_equation);
 }
--- src/rpn.rs
+++ src/rpn.rs
@@ -1,9 +1,19 @@
 
-pub fn rpn_to_infix(expr: &str) {
-    println!("My expression: {}", expr);
+pub fn rpn_to_infix_equation(expr: &str) -> String {
     let tokens_collection = expr.split_whitespace();
     let tokens: Vec<&str> = tokens_collection.collect();
+    let mut n2;
+    let mut n1;
+    let mut stack = vec!["".to_string()];
     for token in tokens {
-        println!("{}", token);
+        if ["+", "-", "*", "/"].contains(&token) {
+            n1 = stack.pop().unwrap().to_string();
+            n2 = stack.pop().unwrap().to_string();
+            let formatted_equation = &format!("({}{}{})", n1, token, n2);
+            stack.push(formatted_equation.to_owned() );
+        } else {
+            stack.push(token.to_string());
+        }
     }
+    return stack.pop().unwrap();
 }
